83f34cec3e00a3cd37f48e0ab6992ab7584a8f6c
JoseCheo <joseche0.ja@gmail.com>
2023-08-23 23:54:11 -0500

    add url

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', // Cambia la URL a la de tu servidor GraphQL
+  uri: 'https://middleware-graphql-node.onrender.com/', // Cambia la URL a la de tu servidor GraphQL
   cache: new InMemoryCache(),
 });
 
